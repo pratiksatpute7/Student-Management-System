f98eaa7ac522c96e4052b8186a5fd14b332b84d9
Shruti Vasave <48850488+shrutivasave@users.noreply.github.com>
2024-04-14 15:05:00 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/LoginSIgnup/loginsignup';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
