1f69fbbb7fc8321d2ac94d464cf0235c8ac542f1
Shruti Vasave <48850488+shrutivasave@users.noreply.github.com>
2024-04-18 13:53:18 -0400

    Add Student commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSIgnup/loginsignup';
-
+import AddStudent from './Components/AddStudent/AddStudent';
 function App() {
   return (
-    <div className="App">
-      <LoginSignup/>
+    //<div className="App">
+    //  <LoginSignup/>
+    //</div>
+    <div className ="App">
+      <AddStudent/>
     </div>
   );
 }
